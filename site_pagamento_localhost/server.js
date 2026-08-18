01311b033a2087fa56d72b5525f465f662d86e8f
Costa <duartecosta563@gmail.com>
2026-08-18 22:48:21 +0100

    Add files via upload

--- site_pagamento_localhost/server.js
+++ site_pagamento_localhost/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const PUBLIC_DIR = path.join(__dirname, 'public');
 const LOG_FILE = path.join(__dirname, 'pagamentos.txt');
 
